331ac40dd88df71903a74fbe69a47f5d369c35f1
Matt Carrington <coffeeslug@gmail.com>
2015-11-18 23:57:10 -0600

    worked on the lists/new view and form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -6,17 +8,36 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def edit
   end
 
   def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list
+    else
+      render 'new'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:id])
+
+  end
+
+  def list_params
+    params.require(:list).permit(:color, :title)
   end
 end
